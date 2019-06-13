8b4542059e2545d77bb9f45adc3a9a9e764b310f
Rachel Zhang <34756692+rachelzhang31@users.noreply.github.com>
2019-06-13 16:28:06 -0700

    made the javascript file better get LEARNIN

---
+++ big.js
@@ -0,0 +1,2 @@
+//you have to include the code for partial matches in JavaScript and then link this file to the main recommendations page I think.
+//Attribute Contains Word is probably going to be pretty useful for this.
