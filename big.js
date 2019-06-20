1a549449cba494dd5b11a9c6cd677d26ed1daeec
Rachel Zhang <34756692+rachelzhang31@users.noreply.github.com>
2019-06-20 14:46:50 -0700

    i guess

--- big.js
+++
@@ -1,2 +0,0 @@
-//you have to include the code for partial matches in JavaScript and then link this file to the main recommendations page I think.
-//Attribute Contains Word is probably going to be pretty useful for this.
